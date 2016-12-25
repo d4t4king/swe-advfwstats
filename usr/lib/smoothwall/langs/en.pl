add57497fbd545dc0c26d3f93d8c82d4214c8caa
dataking <dataking@gmail.com>
2016-12-25 01:47:18 -0800

    Adding the perl modules

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -8,8 +8,8 @@
 	'ssadvfwgraphs'		=>	'Firewal Statistics Graphs',
 	# In page, for translation(s)
 	'afws_global_fw_statistics'		=>	'Global FW Statistics',
-	'afws_filter_green'				=>	'Filter GREEN from sources:',
-	'afws_filter_red'				=>	'Filter RED from sources:',
+	'afws_filter_green'				=>	'Filter <span style="color:#00AA00;font-weight:bold;">GREEN</span> from sources:',
+	'afws_filter_red'				=>	'Filter <span style="color:#AA0000;font-weight:bold;">RED</span> from sources:',
 	'afws_interfaces'				=>	'Interfaces:',
 	'afws_filters'					=>	'Filters:',
 	'afws_sources'					=>	'Sources:',
