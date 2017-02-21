1f3379ec392924393e2c7221b1537a37d6c04011
dataking <dataking@gmail.com>
2017-02-21 11:04:37 -0800

    minor tweaks on names and strings

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -9,7 +9,7 @@
 	# In page, for translation(s)
 	'afws_advanced_stats'			=>	'Advanced FW Statistics',
 	'afws_filter_green'				=>	'Filter <span style="color:#00AA00;font-weight:bold;">GREEN</span> IP from sources:',
-	'afws_filter_red'				=>	'Filter <span style="color:#AA0000;font-weight:bold;">RED</span> IP from sources:',
+	'afws_filter_red'				=>	'Filter <span style="color:#AA0000;font-weight:bold;">RED</span> IP from destinations:',
 	'afws_interfaces'				=>	'Interfaces:',
 	'afws_filters'					=>	'Filters:',
 	'afws_sources'					=>	'Sources:',
