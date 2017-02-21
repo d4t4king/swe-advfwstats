fbfb1c2d2c68e2402ff139b0bac507bb7b061e68
dataking <dataking@gmail.com>
2017-02-21 01:51:44 -0800

    updates

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -4,10 +4,10 @@
 	'advfwstats'					=>	'fw-stats.cgi',
 	'advfwgraphs'					=>	'fw-stat-graphs',
 	# For menu stuffs
-	'ssadvfwstats'					=>	'adv fw stats`',
+	'ssadvfwstats'					=>	'adv fw stats',
 	'ssadvfwgraphs'					=>	'Firewal Statistics Graphs',
 	# In page, for translation(s)
-	'afws_global_fw_statistics'		=>	'Global FW Statistics',
+	'afws_advanced_stats'			=>	'Advanced FW Statistics',
 	'afws_filter_green'				=>	'Filter <span style="color:#00AA00;font-weight:bold;">GREEN</span> IP from sources:',
 	'afws_filter_red'				=>	'Filter <span style="color:#AA0000;font-weight:bold;">RED</span> IP from sources:',
 	'afws_interfaces'				=>	'Interfaces:',
@@ -15,4 +15,5 @@
 	'afws_sources'					=>	'Sources:',
 	'afws_destinations'				=>	'Destinations:',
 	'afws_dest_ports'				=>	'Destination Ports:',
+	'afws_filter_button'			=>	'Filter',
 );
