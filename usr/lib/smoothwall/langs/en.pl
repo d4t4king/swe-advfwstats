d61f53ae7c0ebb319e6281f3e3c05001338cf74d
dataking <dataking@gmail.com>
2017-03-06 07:38:21 -0800

    more translate strings

--- usr/lib/smoothwall/langs/en.pl
+++ usr/lib/smoothwall/langs/en.pl
@@ -2,10 +2,10 @@
 	%basetr,
 	# ???
 	'advfwstats'					=>	'fw-stats.cgi',
-	'advfwgraphs'					=>	'fw-stat-graphs',
+	'advfwgraphs'					=>	'fw-stat-graphs.cgi',
 	# For menu stuffs
 	'ssadvfwstats'					=>	'adv fw stats',
-	'ssadvfwgraphs'					=>	'Firewal Statistics Graphs',
+	'ssadvfwgraphs'					=>	'adv fw stat graphs',
 	# In page, for translation(s)
 	'afws_advanced_stats'			=>	'Advanced FW Statistics',
 	'afws_filter_green'				=>	'Filter <span style="color:#00AA00;font-weight:bold;">GREEN</span> IP from sources:',
